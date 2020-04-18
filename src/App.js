2b7eb0a11aad9370f5459cff8c90c06ead54b479
flareg255 <flare.g255@gmail.com>
2020-04-19 00:42:40 +0900

    three box add

--- src/App.js
+++ src/App.js
@@ -1,17 +1,61 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { Component } from 'react';
+import Box from './Box';
+import { Canvas } from 'react-three-fiber';
+import style from './index.css';
 
-const App = ({initialCount}) => {
-    const [count, setCount] = useState(initialCount);
+class App extends Component{
 
-    return (
-        <>
-            Count: {count}
-            <button onClick={() => setCount(initialCount)}>Reset</button>
-            <button onClick={() => setCount(prevCount => Number(prevCount) - 1)}>-</button>
-            <button onClick={() => setCount(prevCount => Number(prevCount) + 1)}>+</button>
-        </>
-    );
+    constructor(props){
+        super(props);
+
+        this.list = [];
+
+        for(let i = 0; i < 250; i++){
+
+            let listChild = [];
+
+            for(let j = 0; j < 3; j++){
+                if(this.randBetween(1, 10) % 2 === 0){
+                    listChild[j] = this.randBetween(1, 10);
+                }else{
+                    listChild[j] = this.randBetween(1, 10) * -1;
+                }
+                
+            }
+
+            this.list.push(listChild);
+        }
+
+        this.boxList = [];
+
+        for(let data in this.list){
+            console.log(data);
+            this.boxList.push(<Box position={this.list[data]} />)
+        }
+
+        console.log(this.list);
+        console.log(this.boxList);
+    }
+
+    randBetween = (start, end) => {
+        let r = 0;
+        r = Math.floor(Math.random() * end) + start;
+
+        return r;
+    }
+    
+    render(){
+        return (
+            <>
+                <Canvas camera={{ position: [0, 0, 15] }} shadowMap>
+                    <ambientLight />
+                    <pointLight position={[10, 10, 10]} />
+                    <fog attach="fog" args={['#cc7b32', 16, 20]} />
+                    {this.boxList}
+                </Canvas>
+            </>
+        )
+    }
 }
 
 export default App;
